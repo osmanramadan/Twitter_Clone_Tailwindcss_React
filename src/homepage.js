dcfcf29be10cb171ef8ebcdd452c6c27eec24140
osmanramadan <o@o.com>
2024-08-11 17:58:22 +0300

    setup

---
+++ src/homepage.js
@@ -0,0 +1,12 @@
+
+export default function Homepage() {
+
+
+  return (
+    <div className="container mx-auto h-screen flex bg-red-100 xl:max-w-[1200px]">
+      <div className="xl:w-1/5 w-20 h-full flex flex-col bg-blue-100">ossssss</div>
+      <div className="xl:w-1/2 w-full h-full flex flex-col bg-black">098765</div>
+      <div className="flex flex-col">ss</div>
+    </div>
+  )
+}
