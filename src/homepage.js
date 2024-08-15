edca28f15012b3181787e87b40edf0102e847fcb
osmanramadan <o@o.com>
2024-08-15 21:30:03 +0300

    'tailwind-firstscreen'

--- src/homepage.js
+++ src/homepage.js
@@ -1,12 +1,62 @@
 
 export default function Homepage() {
 
-
   return (
-    <div className="container mx-auto h-screen flex bg-red-100 xl:max-w-[1200px]">
-      <div className="xl:w-1/5 w-20 h-full flex flex-col bg-blue-100">ossssss</div>
-      <div className="xl:w-1/2 w-full h-full flex flex-col bg-black">098765</div>
-      <div className="flex flex-col">ss</div>
-    </div>
+      <div className="container  bg-dim-900   mx-auto h-screen  h-full flex  xl:max-w-[1200px]">
+
+        <div className="xl:w-1/4 w-20 flex flex-col text-2xl">
+          <a class="active-link">
+          <i class="fa-brands fa-twitter text-5xl"></i>
+          </a>
+
+          <nav class="mt-3">
+            <a class="active-link">
+            <i class="fa-solid fa-house"></i>
+            <span class="title-link">Home</span>
+            </a>
+            <a class="active-link">
+            <i class=" fa-solid fa-hashtag"></i>
+            <span class="title-link">Explore</span>
+            </a>
+            <a class="active-link">
+            <i class="fa-solid fa-bell"></i>
+            <span class="title-link">Notifications</span>
+            </a>
+            <a class="active-link">
+            <i class="fa-solid fa-envelope"></i>
+            <span class="title-link">Messages</span>
+            </a>
+            <a class="active-link">
+            <i class="fa-solid fa-house"></i>
+            <span class="title-link">Bockmarks</span>
+            </a>
+            <a class="active-link">
+            <i class="fa-solid fa-list-ul"></i>
+            <span class="title-link">Lists</span>
+            </a>
+            <a class="active-link">
+            <i class="fa-solid fa-user"></i>
+            <span class="title-link">Profile</span>
+            </a>
+            <a class="active-link">
+            <i class="fa-solid fa-ellipsis"></i>
+            <span class="title-link">More</span>
+            </a>
+          </nav>
+        </div>
+
+
+        {/* to do */}
+        <div className="xl:w-1/2   w-full  flex flex-col ">098765</div>
+        <div className="xl:w-[30%] hidden  flex flex-col">osmanmamd</div>
+
+      </div>
   )
 }
+
+
+// sm (Small devices):  640px  and  up
+// md (Medium devices): 768px and up
+// lg (Large devices):  1024px and up
+// xl (Extra large devices): 1280px and up
+// 2xl (2X large devices):   1536px   and up
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        dim: {
-          50: "#5F99F7",
-          100: "#5F99F7",
-          200: "#38444d",
-          300: "#202e3a",
-          400: "#253341",
-          500: "#5F99F7",
-          600: "#5F99F7",
-          700: "#192734",
-          800: "#162d40",
-          900: "#15202b",
-        },
-      }
-    },
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const darkMode = 'class';
+export const theme = {
+  extend: {
+    colors: {
+      dim: {
+        50: "#5F99F7",
+        100: "#5F99F7",
+        200: "#38444d",
+        300: "#202e3a",
+        400: "#253341",
+        500: "#5F99F7",
+        600: "#5F99F7",
+        700: "#192734",
+        800: "#162d40",
+        900: "#15202b",
+      },
+    }
   },
-
-  plugins: [],
-}
+};
+export const plugins = [];
