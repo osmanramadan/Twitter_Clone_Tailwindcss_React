5dad0667289f80c7cf7e299b62db530d5695ba81
osmanramadan <o@o.com>
2024-08-22 21:34:57 +0300

    updateright

--- src/homepage.js
+++ src/homepage.js
@@ -2,48 +2,79 @@
 export default function Homepage() {
 
   return (
-      <div className="container  bg-dim-900   mx-auto h-screen  h-full flex  xl:max-w-[1200px]">
+      <div className="mx-11 h-screen  h-full flex  xl:max-w-[1200px]">
 
-        <div className="xl:w-1/4 w-20 flex flex-col text-2xl">
+        <div className="xl:w-1/4   w-20 flex flex-col text-2xl">
+
           <a class="active-link">
-          <i class="fa-brands fa-twitter text-5xl"></i>
+          <i class="fa-solid fa-x text-4xl"></i>
           </a>
 
-          <nav class="mt-3">
+          <nav class="mt-2 mb-3">
+      
             <a class="active-link">
-            <i class="fa-solid fa-house"></i>
-            <span class="title-link">Home</span>
+            <i class="fa-solid title-icon fa-house"></i>
+            <span class="title-link !font-[500]">Home</span>
             </a>
+
             <a class="active-link">
-            <i class=" fa-solid fa-hashtag"></i>
+            <i class=" fa-solid title-icon fa-hashtag"></i>
             <span class="title-link">Explore</span>
             </a>
+
             <a class="active-link">
-            <i class="fa-solid fa-bell"></i>
+            <i class="fa-solid title-icon fa-bell"></i>
             <span class="title-link">Notifications</span>
             </a>
+
             <a class="active-link">
-            <i class="fa-solid fa-envelope"></i>
+            <i class="fa-solid title-icon fa-envelope"></i>
             <span class="title-link">Messages</span>
             </a>
+
             <a class="active-link">
-            <i class="fa-solid fa-house"></i>
+            <i class="fa-solid fa-bookmark title-icon"></i>
             <span class="title-link">Bockmarks</span>
             </a>
+
             <a class="active-link">
-            <i class="fa-solid fa-list-ul"></i>
+            <i class="fa-solid title-icon fa-list-ul"></i>
             <span class="title-link">Lists</span>
             </a>
+
             <a class="active-link">
-            <i class="fa-solid fa-user"></i>
+            <i class="fa-solid title-icon fa-user"></i>
             <span class="title-link">Profile</span>
             </a>
+
             <a class="active-link">
-            <i class="fa-solid fa-ellipsis"></i>
+            <i class="fa-solid  fa-ellipsis"></i>
             <span class="title-link">More</span>
             </a>
           </nav>
+          {/**************/}
+            <a class="active-link !mx-0">
+            <i class="fa-solid fa-feather  icon text-white"></i>
+            </a>
+            <span class="tweet-btn">Post</span>
+          
+          {/**************/}
+          <div class="w-14 xl:w-full mx-auto mt-auto flex items-center  mb-3  p-2 cursor-pointer ">
+          <img class="w-10 !mx-0  h-11 rounded-full"
+            src="/osman.png" alt="person" />
+          <div class="hidden xl:flex flex-col xl:ml-4">
+           <h4 class="text-gray-800 text-white font-[500]">Osman Ramadan</h4>
+           <p  class="text-gray-400 text-xl">@0smanramadan</p>
+          </div>
+          <i class="fa-solid  fa-ellipsis hidden xl:flex items-center xl:ml-5 text-white"></i>
+          </div>
+
         </div>
+        <div class="m-7 flex items-center h-full bg-[#111111]">
+        <span class="h-full w-px bg-[#2F3336]"></span>
+        </div>
+
+
 
 
         {/* to do */}
@@ -60,3 +91,88 @@ export default function Homepage() {
 // lg (Large devices):  1024px and up
 // xl (Extra large devices): 1280px and up
 // 2xl (2X large devices):   1536px   and up
+
+
+// export default function Homepage() {
+
+//   return (
+//       <div className="container  bg-dim-900   mx-auto h-screen  h-full flex  xl:max-w-[1200px]">
+
+//         <div className="xl:w-1/4 w-20 flex flex-col text-2xl">
+//           <a class="active-link">
+//           <i class="fa-brands fa-twitter text-blue-400 text-5xl"></i>
+//           </a>
+
+//           <nav class="mt-2 mb-3">
+//             <a class="active-link">
+//             <i class="fa-solid title-icon fa-house"></i>
+//             <span class="title-link">Home</span>
+//             </a>
+//             <a class="active-link">
+//             <i class=" fa-solid title-icon fa-hashtag"></i>
+//             <span class="title-link">Explore</span>
+//             </a>
+//             <a class="active-link">
+//             <i class="fa-solid title-icon fa-bell"></i>
+//             <span class="title-link">Notifications</span>
+//             </a>
+//             <a class="active-link">
+//             <i class="fa-solid title-icon fa-envelope"></i>
+//             <span class="title-link">Messages</span>
+//             </a>
+//             <a class="active-link">
+//             <i class="fa-solid title-icon fa-house"></i>
+//             <span class="title-link">Bockmarks</span>
+//             </a>
+//             <a class="active-link">
+//             <i class="fa-solid title-icon fa-list-ul"></i>
+//             <span class="title-link">Lists</span>
+//             </a>
+//             <a class="active-link">
+//             <i class="fa-solid title-icon fa-user"></i>
+//             <span class="title-link">Profile</span>
+//             </a>
+//             <a class="active-link">
+//             <i class="fa-solid  fa-ellipsis"></i>
+//             <span class="title-link">More</span>
+//             </a>
+//           </nav>
+//           {/**************/}
+//             <a class="active-link">
+//             <i class="fa-solid fa-feather icon text-white"></i>
+//             </a>
+//             <span class="tweet-btn">Tweet</span>
+          
+//             {/* <!-- User --> */}
+//       <div class="w-14 xl:w-full mx-auto mt-auto flex  justify-between mb-2  p-2 cursor-pointer ">
+//         <img class="w-10 h-10 rounded-full"
+//           src="/osman.png" alt="person" />
+//         <div class="hidden xl:flex flex-col">
+//           <h4 class="text-gray-800 text-white font-bold text-sm">Ag coding</h4>
+//           <p  class="text-gray-400 text-sm">@abdoelazizgamal</p>
+//         </div>
+
+//         <i class="fa-solid fa-chevron-down text-xs hidden xl:flex items-center xl:ml-4  text-gray-800 dark:text-white">
+//         </i>
+
+//       </div>
+
+//         </div>
+
+
+//         {/* to do */}
+//         <div className="xl:w-1/2   w-full  flex flex-col ">098765</div>
+//         <div className="xl:w-[30%] hidden  flex flex-col">osmanmamd</div>
+
+//       </div>
+//   )
+// }
+
+
+// // sm (Small devices):  640px  and  up
+// // md (Medium devices): 768px and up
+// // lg (Large devices):  1024px and up
+// // xl (Extra large devices): 1280px and up
+// // 2xl (2X large devices):   1536px   and up
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,15 @@ export const content = [
 export const darkMode = 'class';
 export const theme = {
   extend: {
+   
     colors: {
-      dim: {
-        50: "#5F99F7",
-        100: "#5F99F7",
-        200: "#38444d",
-        300: "#202e3a",
-        400: "#253341",
-        500: "#5F99F7",
-        600: "#5F99F7",
-        700: "#192734",
-        800: "#162d40",
-        900: "#15202b",
+      blue:{
+        custom:'#1A8CD8'
       },
+      theme:{
+        main:"#000000"
+      }
+
     }
   },
 };
